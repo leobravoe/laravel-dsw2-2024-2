d965f82c9ada940f7ec141874aa6cdf4add9e6b4
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-02-22 15:54:28 -0300

    Adicionando autenticação single user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div class="container">
-        <h2>Sistema Delivery em Laravel</h2>
         <a href="{{ route('produto.index') }}" class="btn btn-primary">Produto</a>
         <a href="{{ route('tipoproduto.index') }}" class="btn btn-primary">TipoProduto</a>
         <a href="{{ route('endereco.index') }}" class="btn btn-primary">Endereço</a>
